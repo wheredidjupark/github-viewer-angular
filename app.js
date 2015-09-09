045007ae4bd4393e55f18d0db8dfe5db148b026b
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-09-09 14:27:47 -0700

    implemented basic user repo information

--- app.js
+++ app.js
@@ -5,8 +5,12 @@
         var onUser = function(response) {
             $scope.user = response.data;
             //console.log(response);
+            $http.get("https://api.github.com/users/"+$scope.username+"/repos").then(onRepos);
         };
 
+        var onRepos = function(response){
+            $scope.repos = response.data;
+        };
         var onError = function(response){
             $scope.error = response;
             //console.log($scope.error.data.message);
