1e96c7ea204e7958a249f8a48c81488c93a396b9
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-09-09 13:47:41 -0700

    display basic user information

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 (function() {
 
-    var MainController = function($scope, $http){
-        $http.get("https://api.github.com/users/wheredidjupark").then(function(response){
+
+    var MainController = function($scope, $http) {
+        var onUser = function(response) {
             $scope.user = response.data;
             console.log(response);
-        });
+        };
+
+        var onError = function(error){
+            $scope.error = error;
+            console.log($scope.error);
+        };
+        $http.get("https://api.github.com/users/wheredidjupark").then(onUser, onError);
     };
 
 
     angular.module("githubViewer", [])
-    .controller("MainController", ["$scope","$http",MainController]);
+        .controller("MainController", ["$scope", "$http", MainController]);
 
 
 
