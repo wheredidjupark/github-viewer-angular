51faa72d2265992fb5a14747161c59337d90438c
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-09-09 17:20:57 -0700

    created github.js service barebone

---
+++ github.js
@@ -0,0 +1,19 @@
+(function(){
+
+	var github = function(){
+
+		var getUser = function(){
+
+		};
+
+		var getRepos = function(){
+
+		};
+		
+		return {
+
+		};
+	};
+	angular.module("githubViewer");
+
+}());
