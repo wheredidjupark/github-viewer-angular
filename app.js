57175ae94c522c0ea3a8c8e0b57fd7e35ce03136
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-10-16 15:02:18 -0700

    implemented a basic server to serve the static files in heroku

---
+++ app.js
@@ -0,0 +1,55 @@
+(function() {
+
+
+    var MainController = function($scope, $interval, $location, $anchorScroll, github) {
+
+        var onUser = function(data) {
+            $scope.user = data;
+            github.getRepos($scope.user).then(onRepos);
+        };
+
+        var onRepos = function(data) {
+            $scope.repos = data;
+            $location.hash("userDetails");
+            $anchorScroll();
+        };
+
+        var onError = function(response) {
+            $scope.error = response;
+        };
+
+        var decrementCount = function() {
+            $scope.count--;
+            if ($scope.count < 1) {
+                $scope.searchUser($scope.username);
+            }
+        };
+
+
+        var countdownInterval = null; //it will store the return promise of the $interval
+        var countdown = function() {
+            countdownInterval = $interval(decrementCount, 1000, $scope.count); //run until the promise is resolved. The promise is resolve when the countdown reaches 
+        };
+
+        $scope.searchUser = function(username) {
+            github.getUser(username).then(onUser, onError);
+            if (countdownInterval) {
+                $interval.cancel(countdownInterval);
+            }
+
+        };
+
+
+        $scope.count = 5;
+        $scope.username = "";
+        countdown();
+    };
+
+
+    angular.module("githubViewer", [])
+        .controller("MainController", ["$scope","$interval","$location","$anchorScroll","github",MainController]);
+
+
+
+
+})();
